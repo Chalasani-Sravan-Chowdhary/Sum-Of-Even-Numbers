9fc6de868adfebefceb9e7ac96deef3c6d83b223
Chalasani Sravan Chowdhary <csc.education.211124@gmail.com>
2025-01-30 16:12:03 +0530

    Add files via upload

---
+++ SumOfEvenNumbersfrom1to1000.java
@@ -0,0 +1,12 @@
+public class SumOfEvenNumbersfrom1to1000 {
+    public static void main(String[] args) {
+        int i= 2;
+        int sum = 0;
+        while(i<=100){
+                sum = sum + i;
+                i+=2;
+        }
+            System.out.println("The sum of all even numbers from 1 to 1000 is "+sum);
+    }
+}
+
